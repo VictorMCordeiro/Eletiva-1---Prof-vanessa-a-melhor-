45e3c8b7ef1c17a778c5f20455a4bcc3a867e6cc
VictorMCordeiro <victorpositivos@gmail.com>
2025-09-02 21:05:33 -0300

    13 e 14
    13 e 14

---
+++ Lista/exer13.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="container mt-4">
+    <h1>Exercicio 13</h1>
+    <h2>Crie um formulário que permita ao usuário inserir um valor em metros. O script PHP deve
+converter esse valor para centímetros e exibir o resultado.</h2>
+
+    <form method="post" action ="exer13resposta.php">   
+        <div class="row">        
+            <div class="mb-3 col-3">
+                <label for="m" class="form-label">Insira valor (Metros) </label>
+                <input type="number" id="m" name="m" class="form-control" required="" placeholder="">
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Calcular para Centimetros</button>        
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>          
